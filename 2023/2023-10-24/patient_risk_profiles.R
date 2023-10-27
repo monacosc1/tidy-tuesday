1c8f2a1cc97a85413d8e25ccc972bc78d2bfe31e
Scott Monaco <monacosc1@gmail.com>
2023-10-27 17:30:03 -0400

    patient risk profiles

---
+++ 2023/2023-10-24/patient_risk_profiles.R
@@ -0,0 +1,44 @@
+### Tidy Tuesday: Patient Risk Profiles ###
+### 10/24/2023 ###
+
+# Load libraries -----------------------------------------------------------
+
+library(tidyverse)
+library(ggtext)
+library(ggplot2)
+library(dplyr)
+library(wordcloud)
+library(tm)
+library(RColorBrewer)
+library(lubridate)
+
+# # add font ----------------------------------------------------------------
+# font_add_google(name = "Roboto Serif", family = "Roboto Serif")
+# font <- "Roboto Serif"
+# 
+# # turn on showtext --------------------------------------------------------
+# showtext_auto()
+# showtext_opts(dpi = 320)
+# 
+# options(scipen = 999) 
+
+# Load data ----------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load('2023-10-24')
+patient_risk_profiles <- tuesdata$patient_risk_profiles
+
+# patient_risk_profiles <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-24/patient_risk_profiles.csv')
+
+# Load fonts ---------------------------------------------------------------
+
+
+# Data wrangling -----------------------------------------------------------
+
+
+# Save gif -----------------------------------------------------------------
+
+
+# Plot ---------------------------------------------------------------------
+
+
+# Plots --------------------------------------------------------------------
